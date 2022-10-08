b7fca679798ca199c428cba518e90f16b285f368
Lanagramme <lborromee16@gmail.com>
2022-10-08 00:23:26 -0400

    aw config module v1

---
+++ config_files/home/.config/awesome/autostart.lua
@@ -0,0 +1,10 @@
+local function autostart(awful)
+	-- {{{ Autostart
+	-- awful.spawn.with_shell("feh --bg-fill -r -z ~/Images/Wallpapers/Landscape/")
+	awful.spawn.with_shell("feh --bg-scale -r -z ~/Images/Wallpapers/Landscape/")
+	awful.spawn.with_shell("compton")
+	awful.spawn.with_shell("~/.config/polybar/launch.sh")
+	-- }}}
+end
+
+return autostart
