2a7f8ebf82550e3f60c2642ab9a3d587ef083e2c
Lanagramme <lborromee16@gmail.com>
2022-12-27 11:54:06 +0100

    replacing the wibar with polybar

--- config_files/home/.config/awesome/autostart.lua
+++ config_files/home/.config/awesome/autostart.lua
@@ -1,8 +1,8 @@
 local function autostart(awful)
 	-- {{{ Autostart
-	-- awful.spawn.with_shell("feh --bg-fill -r -z ~/Images/Wallpapers/Landscape/")
 	awful.spawn.with_shell("feh --bg-scale -r -z ~/Images/Wallpapers/Landscape/")
 	awful.spawn.with_shell("compton")
+	awful.spawn.with_shell("killall -q polybar && polybar || polybar")
 	awful.spawn.with_shell("~/.config/polybar/launch.sh")
 	-- }}}
 end
--- config_files/initialize_dotfiles.sh
+++ config_files/initialize_dotfiles.sh
@@ -2,26 +2,26 @@
 
 #home
 cd ~/
-rm -fr .bash_aliases .bashrc .fonts .gitconfig .profile .tmux.conf .zshrc vimwiki
+rm -fr .bash_aliases .bashrc .fonts .gitconfig .profile .tmux.conf .zshrc vimwiki .bookmarks
 
-ln -s Documents/code/git/config/config_files/home/.bash_aliases ~/
-ln -s Documents/code/git/config/config_files/home/.bashrc ~/
-ln -s Documents/code/git/config/config_files/home/.fonts ~/
-ln -s Documents/code/git/config/config_files/home/.gitconfig ~/
-ln -s Documents/code/git/config/config_files/home/.profile ~/
-ln -s Documents/code/git/config/config_files/home/.tmux.conf ~/
-ln -s Documents/code/git/config/config_files/home/.zshrc ~/
+ln -s ~/Documents/code/git/config/config_files/home/.bash_aliases ~/
+ln -s ~/Documents/code/git/config/config_files/home/.bashrc ~/
+ln -s ~/Documents/code/git/config/config_files/home/.fonts ~/
+ln -s ~/Documents/code/git/config/config_files/home/.gitconfig ~/
+ln -s ~/Documents/code/git/config/config_files/home/.profile ~/
+ln -s ~/Documents/code/git/config/config_files/home/.tmux.conf ~/
+ln -s ~/Documents/code/git/config/config_files/home/.zshrc ~/
 
-ln -s Documents/code/git/config/config_files/home/vimwiki ~/
-ln -s Documents/code/git/config/config_files/home/.bookmarks ~/
+ln -s ~/Documents/code/git/config/config_files/home/vimwiki ~/
+ln -s ~/Documents/code/git/config/config_files/home/.bookmarks ~/
 
 
 #config
 cd ~/.config
 rm -f awesome nvim polybar
 
-ln -s Documents/code/git/config/config_files/home/.config/awesome ~/.config
-ln -s Documents/code/git/config/config_files/home/.config/nvim    ~/.config
-ln -s Documents/code/git/config/config_files/home/.config/polybar ~/.config
+ln -s ~/Documents/code/git/config/config_files/home/.config/awesome ~/.config
+ln -s ~/Documents/code/git/config/config_files/home/.config/nvim    ~/.config
+ln -s ~/Documents/code/git/config/config_files/home/.config/polybar ~/.config
 
 
