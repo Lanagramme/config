6bf29cbd55a3d2c8bf7f7ce85a8e4198a1400eb8
Lanagramme <lborromee16@gmail.com>
2022-12-27 12:17:40 +0100

    nm-applet added to requirements

--- config_files/home/.config/awesome/autostart.lua
+++ config_files/home/.config/awesome/autostart.lua
@@ -2,6 +2,7 @@ local function autostart(awful)
 	-- {{{ Autostart
 	awful.spawn.with_shell("feh --bg-scale -r -z ~/Images/Wallpapers/Landscape/")
 	awful.spawn.with_shell("compton")
+	awful.spawn.with_shell("nm-applet")
 	awful.spawn.with_shell("killall -q polybar && polybar || polybar")
 	awful.spawn.with_shell("~/.config/polybar/launch.sh")
 	-- }}}
