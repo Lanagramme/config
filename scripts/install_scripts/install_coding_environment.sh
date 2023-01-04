b2e3c892e7b0a455c5058f1f8effc1afaba2086e
Lanagramme <lborromee16@gmail.com>
2023-01-04 20:42:28 +0100

    better install process

--- scripts/install_scripts/install_coding_environment.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-apt install cmake make cargo gcc g++ nodejs npm yarn python python3 pip pip3 zsh tmux neovim curl git
---
+++ scripts/install_scripts/readme.txt
@@ -0,0 +1,4 @@
+install script instructions: 
+
+sudo pacman -S $(cat script)
+sudo apt install $(cat script)
