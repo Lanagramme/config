ca1804c67427172eab4d9e8d5b252cddbb0e5ad7
Lanagramme <lborromee16@gmail.com>
2023-01-30 04:40:28 +0100

    ss

--- config_files/home/.config/awesome/rc.lua
+++ config_files/home/.config/awesome/rc.lua
@@ -24,19 +24,13 @@ require("errors")(naughty)
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
--- terminal = "xfce4-terminal"
--- terminal = "terminator"
 -- terminal = "gnome-terminal"
+-- terminal = "sakura"
 terminal = "tilix"
 editor = os.getenv("EDITOR") or "nvim"
 editor_cmd = terminal .. " -e " .. editor
-
--- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
--- If you do not like this or do not have such a key,
--- I suggest you to remap Mod4 to another key using xmodmap or other tools.
--- However, you can use another modifier like Mod1, but it may interact with others.
 modkey = "Mod4"
+
 require ("controls")(gears, awful, hotkeys_popup, menubar)
 require ("tabs")(awful, gears, screen, client)
 require ("rules")(awful, beautiful)
