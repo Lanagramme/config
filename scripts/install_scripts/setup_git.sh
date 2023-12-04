b2f7898c775c17bd427fafd7b406acecc93f8aa9
Lanagramme <lborromee16@gmail.com>
2023-12-04 19:20:50 +0100

    nonono

--- scripts/install_scripts/setup_git.sh
+++ scripts/install_scripts/setup_git.sh
@@ -10,5 +10,5 @@ ssh git -T git@github.com
 
 # go to git and clone config
 mkdir -p ~/Documents/code/git
-git clone git@github.com:Lanagramme/config ~/Documents/code/git/
+git clone git@github.com:Lanagramme/config ~/Documents/code/git/config/
 # ================
