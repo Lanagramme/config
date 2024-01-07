b50a5f809e35973e6b9224c5a6c9bed114f42010
ludji <lborromee16@gmail.com>
2024-01-07 21:00:52 +0100

    ssh

--- scripts/install_scripts/setup_git.sh
+++ scripts/install_scripts/setup_git.sh
@@ -3,6 +3,7 @@
 # installer cle ssh
 unzip ./ssh.zip
 rm -rf ~/.ssh/id_rsa ~/.ssh/id_rsa.pub
+mkdir ~/.ssh
 mv id_rsa id_rsa.pub ~/.ssh
 
 # ssh git
@@ -11,4 +12,7 @@ ssh -T git@github.com
 # go to git and clone config
 mkdir -p ~/Documents/code/git
 git clone git@github.com:Lanagramme/config ~/Documents/code/git/config/
+
+git config --global user.email "lborromee16@gmail.com"
+git config --global user.user "Lanagramme"
 # ================
