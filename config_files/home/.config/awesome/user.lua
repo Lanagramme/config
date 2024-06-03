39eacf0263688a582946d8a9175e9dd4a057d096
Lanagramme <lborromee16@gmail.com>
2024-06-03 14:24:33 +0200

    alacritty config

--- config_files/home/.config/awesome/user.lua
+++ config_files/home/.config/awesome/user.lua
@@ -1,3 +1,10 @@
+local function command_exists(command)
+    local handle = io.popen("command -v " .. command .. " >/dev/null 2>&1 && echo 1 || echo 0")
+    local result = handle:read("*a")
+    handle:close()
+    return tonumber(result) == 1
+end
+
 -- browser = "firefox-esr"
 browser = "firefox"
 
@@ -9,9 +16,20 @@ file_manager = "nautilus"
 -- terminal = "sakura"
 -- terminal = "tilix"
 -- terminal = "kitty"
-terminal = "alacritty"
+-- terminal = "alacritty"
+
+
+modkey = "Mod4"
+
+-- Set the terminal based on availability
+if command_exists("kitty") then
+    terminal = "kitty"
+elseif command_exists("alacritty") then
+    terminal = "alacritty"
+else
+    -- Fallback to another terminal if neither is available
+    terminal = "gnome-terminal"
+end
 
 editor = os.getenv("EDITOR") or "nvim"
 editor_cmd = terminal .. " -e " .. editor
-
-modkey = "Mod4"
