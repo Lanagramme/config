39ac2e4ef319de56111e0d05aa17551336ab24c7
Lanagramme <lborromee16@gmail.com>
2024-09-06 15:56:32 +0200

    code

--- save/battle/main.py
+++ save/battle/main.py
@@ -1,36 +1,47 @@
 import pygame
 import numpy as np
+from random import randrange
 
-WHITE = (255,255,255)
-BLUE  = (0,0,255)
+WHITE = (255,241,215)
+BLUE  = (93,100,190)
 GREEN = (0,255,0)
-RED   = (255,0,0)
+
+RED   = (250,170,117)
+DARK  = (245,121,85)
+
+PURPLE = (125,82,222)
+CARMIN = (120,227,253)
 
 class Pion:
-  def __init__(self, team):
+  def __init__(self, team, position):
     self.team = team
-    self.position
+    self.position = position
 
 class Cell:
   def __init__(self, x, y, margin, gutter, size, hover):
     self.x = x
     self.y = y
+    self.padding = 5
     self.dimensions = (size, size)
     self.body = pygame.Rect(
       (margin + x * (size + gutter), margin + y * (size + gutter)),
       self.dimensions
     )
     self.hover = hover
-    self.color = self.is_hovered() 
+    self.color = self.def_color() 
   
-  def is_hovered(self):
+  def def_color(self):
     if self.hover != False and (self.x,self.y) == self.hover:
-      return (200,0,0)
+      return DARK
     return RED
-    
-    
-  def draw(self, SCREEN):
+       
+  def draw(self, SCREEN, has_pion, pion_color):
     pygame.draw.rect(SCREEN, self.color, self.body)
+    if has_pion:
+      pygame.draw.circle(
+        SCREEN, pion_color,
+        (self.body.centerx, self.body.centery), self.body.width /2 -self.padding
+      )
 
 class Grid:
   def __init__(self, width, height, margin, gutter, size):
@@ -51,36 +62,48 @@ class Grid:
     self.cells = [[False for _ in range(self.Y)] for _ in range(self.X)]
 
   def paint(self, SCREEN):
-    X = self.X
-    Y = self.Y
-
-    def draw(x, y, margin, gutter, size, hover, SCREEN):
-      Cell(x, y, margin, gutter, size, hover).draw(SCREEN)
-      pygame.draw.circle(
-        SCREEN, BLUE, 
-        ((x*(size+gutter)+margin)+size/2, ( y*(size+gutter)+margin )+size/2), size/2-5)
-    
-    [
-      draw(x, y, self.margin, self.gutter, self.size, self.hover,SCREEN)
-      for x in range(X)
-      for y in range(Y)
-    ]
+    for x in range(self.X):
+      for y in range(self.Y):
+        has_pion = self.cells[x][y] is not False
+        pion_color = TEAMS[LIST_PIONS[self.cells[x][y]].team]["color"] if has_pion else None
+        cell = Cell(x,y,self.margin,self.gutter,self.size,self.hover)
+        cell.draw(SCREEN, has_pion, pion_color)
 
 game = Grid(9, 6, 50, 1, 50)
+board = pygame.Rect(game.margin, game.margin, game.spanX, game.spanY)
 
 pygame.init()
+pygame.time.Clock().tick(60)
+
 HEIGHT = game.margin * 2 + game.spanY
 WIDTH = game.margin * 2 + game.spanX
 SIZE = (WIDTH, HEIGHT)
 SCREEN = pygame.display.set_mode(SIZE)
-PIONS = []
+LIST_PIONS = []
 
 TEAMS = {
-  team1 = { color = PURPLE, pions = [] },
-  team2 = { color = CARMIN, pions = [] }
+  "team1" : { "color" : PURPLE, "pions" : [] },
+  "team2" : { "color" : CARMIN, "pions" : [] }
 }
 
-board = pygame.Rect(game.margin, game.margin, game.spanX, game.spanY)
+def create(team):
+  while True:
+    X = randrange(game.X)
+    Y = randrange(game.Y)
+    pos = (X,Y)
+    nouveau_pion = Pion(team,pos)
+    if game.cells[X][Y] == False: 
+      break
+  
+  game.cells[X][Y] = len(LIST_PIONS)
+  LIST_PIONS.append(nouveau_pion)
+
+create("team1")
+create("team1")
+create("team1")
+create("team2")
+create("team2")
+create("team2")
 
 playing = True
 while playing:
@@ -97,6 +120,8 @@ while playing:
   else:
     game.hover = False
     
-  SCREEN.fill((255,255,255))
+  SCREEN.fill(WHITE)
   game.paint(SCREEN)
   pygame.display.flip()
+
+pygame.quit()
