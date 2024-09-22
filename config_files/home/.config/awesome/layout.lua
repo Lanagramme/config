6136dbb951f533e82863acae1264257eb9d97023
Lanagramme <lborromee16@gmail.com>
2024-09-23 01:28:54 +0200

    screen padding

--- config_files/home/.config/awesome/layout.lua
+++ config_files/home/.config/awesome/layout.lua
@@ -1,48 +1,58 @@
 pcall(require, "luarocks.loader")
 
-local wibox = require 'wibox'
-local awful = require 'awful'
-local beautiful  = require 'beautiful'
-local gears  = require 'gears'
-local menubar  = require 'menubar'
+local wibox = require("wibox")
+local awful = require("awful")
+local beautiful = require("beautiful")
+local gears = require("gears")
+local menubar = require("menubar")
 
 --local function Screen(hotkeys_popup)
 
-	-- Table of layouts to cover with awful.layout.inc, order matters.
-	awful.layout.layouts = {
-		-- awful.layout.suit.floating,
-		awful.layout.suit.tile,
-		awful.layout.suit.tile.left,
-		-- awful.layout.suit.tile.bottom,
-		-- awful.layout.suit.tile.top,
-		awful.layout.suit.fair,
-		-- awful.layout.suit.fair.horizontal,
-		awful.layout.suit.spiral,
-		-- awful.layout.suit.spiral.dwindle,
-		-- awful.layout.suit.max,
-		-- awful.layout.suit.max.fullscreen,
-		-- awful.layout.suit.magnifier,
-		-- awful.layout.suit.corner.nw,
-		-- awful.layout.suit.corner.ne,
-		-- awful.layout.suit.corner.sw,
-		-- awful.layout.suit.corner.se,
+-- Table of layouts to cover with awful.layout.inc, order matters.
+awful.layout.layouts = {
+	-- awful.layout.suit.floating,
+	awful.layout.suit.tile,
+	awful.layout.suit.tile.left,
+	-- awful.layout.suit.tile.bottom,
+	-- awful.layout.suit.tile.top,
+	awful.layout.suit.fair,
+	-- awful.layout.suit.fair.horizontal,
+	awful.layout.suit.spiral,
+	-- awful.layout.suit.spiral.dwindle,
+	-- awful.layout.suit.max,
+	-- awful.layout.suit.max.fullscreen,
+	-- awful.layout.suit.magnifier,
+	-- awful.layout.suit.corner.nw,
+	-- awful.layout.suit.corner.ne,
+	-- awful.layout.suit.corner.sw,
+	-- awful.layout.suit.corner.se,
+}
+-- }}}
+
+awful.screen.connect_for_each_screen(function(s)
+	-- Each screen has its own tag table.
+	awful.tag({ "1", "2", "3", "4", "5" }, s, awful.layout.layouts[1])
+
+	s.mylayoutbox = awful.widget.layoutbox(s)
+	s.padding = {
+		left = -10,
 	}
-	-- }}}
-
-	awful.screen.connect_for_each_screen(function(s)
-		-- Each screen has its own tag table.
-		awful.tag({ "1", "2", "3", "4", "5"}, s, awful.layout.layouts[1])
-
-		s.mylayoutbox = awful.widget.layoutbox(s)
-		s.mylayoutbox:buttons(gears.table.join(
-			awful.button({ }, 1, function () awful.layout.inc( 1) end),
-			awful.button({ }, 3, function () awful.layout.inc(-1) end),
-			awful.button({ }, 4, function () awful.layout.inc( 1) end),
-			awful.button({ }, 5, function () awful.layout.inc(-1) end)
-		))
-	end)
-	-- }}}
-
+	s.mylayoutbox:buttons(gears.table.join(
+		awful.button({}, 1, function()
+			awful.layout.inc(1)
+		end),
+		awful.button({}, 3, function()
+			awful.layout.inc(-1)
+		end),
+		awful.button({}, 4, function()
+			awful.layout.inc(1)
+		end),
+		awful.button({}, 5, function()
+			awful.layout.inc(-1)
+		end)
+	))
+end)
+-- }}}
 
 --end
 --return Screen
